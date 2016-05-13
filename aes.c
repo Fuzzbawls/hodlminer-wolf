027bf0013ae8bf20b14161430fe5d052fc22b0d5
Fuzzbawls <fuzzbawls@gmail.com>
2016-05-12 21:25:23 -0700

    Add Travis

--- aes.c
+++ aes.c
@@ -1,34 +1,6 @@
 #include <stdint.h>
 #include <x86intrin.h>
 
-#ifdef __APPLE__
-#include <libkern/OSByteOrder.h>
-
-#define htobe16(x) OSSwapHostToBigInt16(x)
-#define htole16(x) OSSwapHostToLittleInt16(x)
-#define be16toh(x) OSSwapBigToHostInt16(x)
-#define le16toh(x) OSSwapLittleToHostInt16(x)
- 
-#define htobe32(x) OSSwapHostToBigInt32(x)
-#define htole32(x) OSSwapHostToLittleInt32(x)
-#define be32toh(x) OSSwapBigToHostInt32(x)
-#define le32toh(x) OSSwapLittleToHostInt32(x)
- 
-#define htobe64(x) OSSwapHostToBigInt64(x)
-#define htole64(x) OSSwapHostToLittleInt64(x)
-#define be64toh(x) OSSwapBigToHostInt64(x)
-#define le64toh(x) OSSwapLittleToHostInt64(x)
-
-#define __BYTE_ORDER    BYTE_ORDER
-#define __BIG_ENDIAN    BIG_ENDIAN
-#define __LITTLE_ENDIAN LITTLE_ENDIAN
-#define __PDP_ENDIAN    PDP_ENDIAN
-
-#define _mm256_set_m128i(v0, v1)  _mm256_insertf128_si256(_mm256_castsi128_si256(v1), (v0), 1)
-
-#define _mm256_setr_m128i(v0, v1) _mm256_set_m128i((v1), (v0))
-#endif // __APPLE__
-
 static inline void ExpandAESKey256_sub1(__m128i *tmp1, __m128i *tmp2)
 {
 	__m128i tmp4;
